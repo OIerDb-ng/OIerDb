0bf80192f27f6dd76761201e32e7004bc915e6b0
Baoshuo <i@baoshuo.ren>
2021-12-12 15:36:42 +0800

    feat: give an error page if not support indexedDB

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
-import { Container } from 'semantic-ui-react';
+import { Container, Header, Message, Icon } from 'semantic-ui-react';
+
+const notSupportIndexedDB = !globalThis || !globalThis.indexedDB;
+
+const App = () => {
+    if (notSupportIndexedDB) {
+        return (
+            <div style={{ padding: '1.5rem 0' }}>
+                <Header as="h3" dividing>
+                    请更新浏览器
+                </Header>
+                <div>
+                    <Message error>
+                        <Icon name="remove" />
+                        您的浏览器不支持 <code>indexedDB</code>。
+                    </Message>
+                    <p>
+                        推荐使用最新版的{' '}
+                        <a href="https://www.google.cn/chrome/" target="_blank">
+                            Google Chrome 浏览器
+                        </a>{' '}
+                        访问 OIerDB NG。
+                    </p>
+                </div>
+            </div>
+        );
+    }
+    return (
+        <>
+            <div>Hello World!</div>
+        </>
+    );
+};
 
 ReactDOM.render(
     <React.StrictMode>
         <Container>
-            <div>Hello World!</div>
+            <App />
         </Container>
     </React.StrictMode>,
     document.getElementById('app')
