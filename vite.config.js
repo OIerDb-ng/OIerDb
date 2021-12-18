f8174c0d98c13bbab2fd8adc1cbe74b63ac77f1a
Baoshuo <i@baoshuo.ren>
2021-12-18 18:52:11 +0800

    feat: minify html on build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { minifyHtml } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react(), minifyHtml()],
+});
