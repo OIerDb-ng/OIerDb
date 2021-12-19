cb842459b007f7479c33634ff1ef55edaf794110
Baoshuo <i@baoshuo.ren>
2021-12-19 11:14:14 +0800

    feat: add logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Container, Icon, Menu } from 'semantic-ui-react';
 
+// Logo
+import logo from '../../assets/logo-white.png';
+
 export default () => (
     <>
         <Menu fixed="top" borderless>
             <Container>
                 <Menu.Item>
+                    <img src={logo} style={{ marginRight: '0.75em' }} />
                     <div style={{ fontSize: '20px' }}>
                         <b>OIerDb</b>
                     </div>
