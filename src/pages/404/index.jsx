5ca7ea6c939131dcc314f171d06eac09bddd2ac4
Baoshuo <i@baoshuo.ren>
2021-12-21 21:39:41 +0800

    feat: add a link to return to home in 404 page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,5 +1,6 @@
 // Components
 import { Message, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 export default () => (
     <>
@@ -8,6 +9,7 @@ export default () => (
             <Message.Content>
                 <Message.Header>404</Message.Header>
                 未找到请求的页面。
+                <Link to="/">返回首页</Link>
             </Message.Content>
         </Message>
     </>
