682e4131ccfed66a232f51edd178c4d1a9a37647
Baoshuo <i@baoshuo.ren>
2021-12-22 20:48:33 +0800

    revert: b3856e122433b11160235cf3f248f6308ec60b72.

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
