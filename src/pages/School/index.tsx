345284645047defcdc482f77a915f8484b23f2d4
Baoshuo <i@baoshuo.ren>
2021-12-26 10:51:33 +0800

    feat: add schools rank list

--- src/pages/School/index.tsx
+++ src/pages/School/index.tsx
@@ -1,7 +1,104 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+// Components
+import { Pagination, Table, Icon } from 'semantic-ui-react';
+
+// Utils
+import { useScreenWidthWithin } from '@/utils/useScreenWidthWithin';
+
+const SchoolRank: React.FC = () => {
+    const [page, setPage] = useState(1);
+    const totalPages = Math.ceil(OIerDb.schools.length / 30);
+
+    const screenWidthLessThan376 = useScreenWidthWithin(0, 376);
+    const screenWidthLessThan450 = useScreenWidthWithin(0, 450);
+    const screenWidthLessThan680 = useScreenWidthWithin(0, 680);
+    const screenWidthLessThan768 = useScreenWidthWithin(0, 768);
+    const screenWidthLessThan1024 = useScreenWidthWithin(0, 1024);
+
+    let siblingRange: number, size: string;
+    if (screenWidthLessThan376) {
+        siblingRange = 0;
+        size = 'small';
+    } else if (screenWidthLessThan450) {
+        siblingRange = 0;
+    } else if (screenWidthLessThan680) {
+        siblingRange = 1;
+    } else if (screenWidthLessThan768) {
+        siblingRange = 2;
+    } else if (screenWidthLessThan1024) {
+        siblingRange = 3;
+    } else {
+        siblingRange = 4;
+    }
+
+    return (
+        <>
+            <h2>全国信息学竞赛学校排名</h2>
+            <Table celled unstackable>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>#</Table.HeaderCell>
+                        <Table.HeaderCell>学校</Table.HeaderCell>
+                        <Table.HeaderCell>省份</Table.HeaderCell>
+                        <Table.HeaderCell>评分</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {OIerDb.schools
+                        .slice(30 * (page - 1), 30 * page)
+                        .map((school) => (
+                            <Table.Row>
+                                <Table.Cell>{school.rank + 1}</Table.Cell>
+                                <Table.Cell>
+                                    <Link
+                                        to={'/school/' + school.id}
+                                        style={{ color: 'inherit' }}
+                                    >
+                                        {school.name}
+                                    </Link>
+                                </Table.Cell>
+                                <Table.Cell>{school.province}</Table.Cell>
+                                <Table.Cell>{school.score}</Table.Cell>
+                            </Table.Row>
+                        ))}
+                </Table.Body>
+            </Table>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Pagination
+                    firstItem={null}
+                    lastItem={null}
+                    size={size}
+                    siblingRange={siblingRange}
+                    ellipsisItem={{
+                        content: '...',
+                        disabled: true,
+                        icon: true,
+                    }}
+                    prevItem={{
+                        content: <Icon name="angle left" />,
+                        icon: true,
+                        disabled: page === 1,
+                    }}
+                    nextItem={{
+                        content: <Icon name="angle right" />,
+                        icon: true,
+                        disabled: page === totalPages,
+                    }}
+                    totalPages={totalPages}
+                    onPageChange={(_, data) =>
+                        setPage(data.activePage as number)
+                    }
+                />
+            </div>
+        </>
+    );
+};
+
 export const School: React.FC = () => (
     <>
         {/* TODO: 学校搜索 */}
-        {/* TODO: 全国信息学竞赛学校排名 */}
-        正在施工中...
+        <SchoolRank />
     </>
 );
