e305d1391b20ebac723634de922df6d68feae2a4
Baoshuo Ren <i@baoshuo.ren>
2022-01-17 10:02:12 +0800

    fix(utils/getGrade): display grade by school year

--- src/utils/getGrade.ts
+++ src/utils/getGrade.ts
@@ -1,5 +1,7 @@
 export default (enroll_middle: number, contestYear?: number) => {
-    const nowYear = contestYear || new Date().getFullYear();
+    const nowYear =
+        contestYear ||
+        new Date().getFullYear() - Number(new Date().getMonth() + 1 < 9);
     const years1 = ['初一', '初二', '初三', '高一', '高二', '高三'];
     const years2 = ['六年级', '五年级', '四年级', '三年级', '二年级', '一年级'];
 
