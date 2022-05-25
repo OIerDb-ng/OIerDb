a1904878dc4a983016316bdd59ad6c240c232daa
Baoshuo <i@baoshuo.ren>
2022-05-25 08:48:35 +0800

    chore: use external resources

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { minifyHtml, injectHtml } from 'vite-plugin-html';
+import { viteExternalsPlugin } from 'vite-plugin-externals';
 import path from 'path';
 import crypto from 'node:crypto';
+
+const isProd = process.env.NODE_ENV === 'production' && !process.env.NETLIFY;
 
 const buildHash =
   /* Netlify */ process.env.COMMIT_REF ||
@@ -19,7 +22,110 @@ const data = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), minifyHtml(), injectHtml({ data })],
+  plugins: [
+    react(),
+    injectHtml({
+      data,
+      tags: [
+        {
+          injectTo: 'head',
+          tag: 'script',
+          attrs: {
+            src: '/OIerDb.js',
+          },
+        },
+        {
+          injectTo: 'head',
+          tag: 'script',
+          attrs: {
+            src: '//oier-data.baoshuo.dev/static.js',
+          },
+        },
+        {
+          injectTo: 'head',
+          tag: 'script',
+          attrs: {
+            src: '//oier-data.baoshuo.dev/result.sha512.js',
+          },
+        },
+        {
+          injectTo: 'head',
+          tag: 'link',
+          attrs: {
+            rel: 'stylesheet',
+            href: '//cdnjs.baoshuo.ren/ajax/libs/semantic-ui/2.4.1/semantic.min.css',
+          },
+        },
+        {
+          injectTo: 'head',
+          tag: 'script',
+          attrs: {
+            defer: true,
+            'data-domain': 'oier.baoshuo.dev',
+            src: '//stat.u.sb/js/script.js',
+          },
+        },
+        ...(isProd
+          ? [
+              {
+                injectTo: 'head',
+                tag: 'script',
+                attrs: {
+                  src: '//cdnjs.baoshuo.ren/ajax/libs/react/17.0.2/umd/react.production.min.js',
+                },
+              },
+              {
+                injectTo: 'head',
+                tag: 'script',
+                attrs: {
+                  src: '//cdnjs.baoshuo.ren/ajax/libs/react-dom/17.0.2/umd/react-dom.production.min.js',
+                },
+              },
+              {
+                injectTo: 'head',
+                tag: 'script',
+                attrs: {
+                  src: '//cdnjs.baoshuo.ren/ajax/libs/react-router-dom/6.2.1/react-router-dom.production.min.js',
+                },
+              },
+              {
+                injectTo: 'head',
+                tag: 'script',
+                attrs: {
+                  src: '//cdnjs.baoshuo.ren/ajax/libs/Chart.js/3.7.0/chart.min.js',
+                },
+              },
+              {
+                injectTo: 'head',
+                tag: 'script',
+                attrs: {
+                  src: '',
+                },
+              },
+            ]
+          : [
+              {
+                injectTo: 'head-prepend',
+                tag: 'meta',
+                attrs: {
+                  name: 'referrer',
+                  content: 'no-referrer',
+                },
+              },
+            ]),
+      ],
+    }),
+    minifyHtml(),
+    viteExternalsPlugin(
+      {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        'react-router-dom': 'ReactRouterDOM',
+        'chart.js': 'Chart',
+      },
+      { disableInServe: true }
+    ),
+  ],
   build: {
     sourcemap: true,
   },
