7985ce4602ac2a00a4836e395788d14121190a83
Baoshuo <i@baoshuo.ren>
2022-08-07 08:51:10 +0800

    feat: contestant filter

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -11,6 +11,7 @@ import {
 } from 'semantic-ui-react';
 import PersonCard from '@/components/PersonCard';
 import getGrade, { currentYear } from '@/utils/getGrade';
+import compareGrades from '@/utils/compareGrades';
 import { provinces, type OIer } from '@/libs/OIerDb';
 import styles from './Search.module.less';
 
@@ -153,28 +154,7 @@ const Search: React.FC = () => {
                     value: year,
                     text: getGrade(year, currentYear),
                   }))
-                  .sort((a, b) => {
-                    const t = ['一', '二', '三', '四', '五', '六'];
-
-                    let keyA = a.key;
-                    let keyB = b.key;
-
-                    // 高中
-                    if (a.text[0] === '高' && !a.text.includes('毕业'))
-                      keyA *= 4;
-                    if (b.text[0] === '高' && !b.text.includes('毕业'))
-                      keyB *= 4;
-
-                    // 初中
-                    if (a.text[0] === '初') keyA *= 3;
-                    if (b.text[0] === '初') keyB *= 3;
-
-                    // 小学
-                    if (t.includes(a.text[0])) keyA *= 2;
-                    if (t.includes(b.text[0])) keyB *= 2;
-
-                    return keyB - keyA;
-                  })}
+                  .sort(compareGrades)}
                 defaultValue={grade}
                 onChange={(_, { value }) => setGrade(value as string)}
               />
