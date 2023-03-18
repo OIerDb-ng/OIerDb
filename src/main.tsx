f0a4c06b18f2f96c2ea079772602ed578dce730c
Baoshuo <i@baoshuo.ren>
2023-03-18 16:24:54 +0800

    fix: eslint

--- src/main.tsx
+++ src/main.tsx
@@ -38,7 +38,7 @@ const notSupportIndexedDB = !globalThis || !globalThis.indexedDB;
 // 取消注册先前的 Service Worker
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.getRegistrations().then((registrations) => {
-    for (let registration of registrations) {
+    for (const registration of registrations) {
       registration.unregister();
     }
   });
