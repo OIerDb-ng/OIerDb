e7a15afd1a3904e4324533d070369c2afcc09fdf
Baoshuo <i@baoshuo.ren>
2025-10-09 20:43:20 +0800

    backend: use pino logger

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
 
 import { OIerDbModule } from './common/oierdb/oierdb.module';
 import { OierModule } from './oier/oier.module';
 
 @Module({
-  imports: [OIerDbModule, OierModule],
+  imports: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        transport: process.env.NODE_ENV !== 'production' ? { target: 'pino-pretty' } : undefined,
+      },
+    }),
+    OIerDbModule,
+    OierModule,
+  ],
 })
 export class AppModule {}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,13 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger } from 'nestjs-pino';
 
 import { AppModule } from './app.module';
 
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { bufferLogs: true });
+
+  app.useLogger(app.get(Logger));
 
   app.useGlobalPipes(
     new ValidationPipe({
