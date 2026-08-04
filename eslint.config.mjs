70958a151cf4a45bafa80afa610814f1ddda39e5
Baoshuo <i@baoshuo.ren>
2026-08-04 17:12:30 +0800

    workspace: eslint no-unused-vars whitelist

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -66,6 +66,18 @@ export default tseslint.config(
           ignoreRegExpLiterals: true,
         },
       ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
     },
-  },
+  }
 );
